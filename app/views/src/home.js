ac1635798a3e793643d5de4fa7bb0559fdc8ef5a
theflyingmantis <rai.1@iitj.ac.in>
2018-01-21 08:17:45 +0530

    0.0.0.0 done

--- app/views/src/home.js
+++ app/views/src/home.js
@@ -1,5 +1,5 @@
 console.log('JS is working');
-var socket = io('http://localhost:3000');
+var socket = io('http://0.0.0.0:3000');
 socket.on('orderReceived',function(data){
   var node = document.createElement("LI");
   var textnode = document.createTextNode(data);
